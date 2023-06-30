a6ce4b719eb23d7aca194a85d14ba92fbf7569e8
Glenn Louédec <glouedec@centrale-marseille.fr>
2023-07-01 00:11:02 +0900

    add fixtures and moved some things around

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ Student scalarDb project for Keio University Course
 ## Installation
 First install docker on your computer
 
-To launch the databases run ```docker-compose up```
+To launch the databases run ```docker compose up -d```
 
-To see the ip of your databases, use ```docker ps```
+To load the database you can do ```$ java -jar scalardb-schema-loader-<VERSION>.jar --config database.properties --schema-file schema.json --coordinator```
 
-(You can also use docker portainer, it will create a stack of two containers)
+After that to launch to server you can do ````./gradlew run``` or ```gradlew.bat run```
+
+If you want to stop the containers use ```docker compose down```
--- src/main/java/scalibrary/AppMain.java
+++ src/main/java/scalibrary/AppMain.java
@@ -1,7 +1,26 @@
 package scalibrary;
 
+import com.scalar.db.api.DistributedTransactionManager;
+import com.scalar.db.service.TransactionFactory;
+import com.scalar.db.exception.transaction.TransactionException;
+import java.io.IOException;
+
+
 public class AppMain {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+  public static void main(String[] args) {
+    try(DataLoader loader = new DataLoader()){
+      DistributedTransactionManager manager = loader.getManager();
+      // Loading the fake data
+      try {
+        loader.loadInitialData();
+        loader.loadFixtureData();
+      } catch (TransactionException e) {
+        e.printStackTrace();
+      }
+      // Creating the server with javalin
+          
+    }catch (IOException e){
+      e.printStackTrace();
     }
+  } 
 }
